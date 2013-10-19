7b74986132ba5e165c67fbfcbc41a5da67f00482
Francesco Bailo <me@francescobailo.net>
2013-10-19 21:15:39 +1100

    Tabbed and unique file

--- OPAC_SBN.js
+++ OPAC_SBN.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "g",
-	"lastUpdated": "2013-10-19 15:51:14"
+	"lastUpdated": "2013-10-19 21:10:43"
 }
 
 /*
@@ -33,104 +33,104 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 function detectWeb(doc, url) {
-  if (doc.title.match("Scheda dettagliata")) {
+	if (doc.title.match("Scheda dettagliata")) {
 	return "book";
-  } else if (doc.title.match("Risultati sintetici")) {
+	} else if (doc.title.match("Risultati sintetici")) {
 	return "multiple";
-  }
+	}
 }
 
 // The function used to save well formatted data to Zotero
 function associateData (newItem, items, field, zoteroField) {
-  if (items[field]) {
+	if (items[field]) {
 	newItem[zoteroField] = items[field];
-  }
+	}
 }
 
 function scrape(doc, url) {
-  // variable declarations
-  var newItem = new Zotero.Item('book');
-  newItem.url = doc.location.href;
-  newItem.title = "No Title Found";
-  var items = new Object();
-  var tagsContent = new Array();
+	// variable declarations
+	var newItem = new Zotero.Item('book');
+	newItem.url = doc.location.href;
+	newItem.title = "No Title Found";
+	var items = new Object();
+	var tagsContent = new Array();
 
-  // scrape page data, save to Zotero
-  getItems(doc, items, tagsContent);
-  getAuthors(newItem, items);
-  getTitle(newItem, items);
-  getImprints(newItem, items);
-  getPages(newItem, items);
-  getEdition(newItem, items);
-  saveToZotero(newItem, items);
+	// scrape page data, save to Zotero
+	getItems(doc, items, tagsContent);
+	getAuthors(newItem, items);
+	getTitle(newItem, items);
+	getImprints(newItem, items);
+	getPages(newItem, items);
+	getEdition(newItem, items);
+	saveToZotero(newItem, items);
 }
 
 function getItems(doc, items, tagsContent) {
-  // namespace code
-  var namespace = doc.documentElement.namespaceURI;
-  var nsResolver = namespace ? 
+	// namespace code
+	var namespace = doc.documentElement.namespaceURI;
+	var nsResolver = namespace ? 
 	function(prefix) {
-	  if (prefix == 'x') return namespace; else return null;
+		if (prefix == 'x') return namespace; else return null;
 	} : null;
 
-  // populate "items" Object and save tags to an Array
-  var blankCell = "temp";
-  var headersTemp;
-  var headers;
-  var contents;
-  var myXPathObject = doc.evaluate('//td[1]', doc, nsResolver, XPathResult.ANY_TYPE, null);
-  var myXPathObject2 = doc.evaluate('//td[2]', doc, nsResolver, XPathResult.ANY_TYPE, null);
-  while (headers = myXPathObject.iterateNext()) {
+	// populate "items" Object and save tags to an Array
+	var blankCell = "temp";
+	var headersTemp;
+	var headers;
+	var contents;
+	var myXPathObject = doc.evaluate('//td[1]', doc, nsResolver, XPathResult.ANY_TYPE, null);
+	var myXPathObject2 = doc.evaluate('//td[2]', doc, nsResolver, XPathResult.ANY_TYPE, null);
+	while (headers = myXPathObject.iterateNext()) {
 	headersTemp = headers.textContent;
 	if (!headersTemp.match(/\w/)) {
-	  headersTemp = blankCell;
-	  blankCell = blankCell + "1";
+		headersTemp = blankCell;
+		blankCell = blankCell + "1";
 	}
 	contents = myXPathObject2.iterateNext().textContent;
 	items[headersTemp.replace(/\s+/g, '')]=contents.replace(/^\s*|\s*$/g, '');
-  }
+	}
 }
 
 function getAuthors(newItem, items) {
-  //Formatting and saving "Author" field
-  if (items["Autoreprincipale"]) {
+	//Formatting and saving "Author" field
+	if (items["Autoreprincipale"]) {
 	var author = items["Autoreprincipale"];
 	var words = author.split(", ");
 	var authorFixed = '';
 	for (i = words.length-1; i > -1; i--) {
-	  words[i] = words[i].replace(/\W/g, '');
-	  words[i] = words[i].replace(/\d/g, '');
-	  authorFixed = authorFixed + words[i] + ' ';
-	  author = authorFixed;
+		words[i] = words[i].replace(/\W/g, '');
+		words[i] = words[i].replace(/\d/g, '');
+		authorFixed = authorFixed + words[i] + ' ';
+		author = authorFixed;
 	}
-  } else {
-		author = items["Titolo"].substr(items["Titolo"].indexOf("/") + 1);
-  }
-  newItem.creators.push(Zotero.Utilities.cleanAuthor(author, "author"));
+	} else {
+	author = items["Titolo"].substr(items["Titolo"].indexOf("/") + 1);
+	}
+	newItem.creators.push(Zotero.Utilities.cleanAuthor(author, "author"));
 }
 
 function getTitle(newItem, items) {
-  if (items["Titolo"]) {
-  	var title = items["Titolo"];
-  	if (items["Titolo"].match("/")) {
-  	  title = items["Titolo"].substr(0,items["Titolo"].indexOf("/"));  
-  	}
+	if (items["Titolo"]) {
+	var title = items["Titolo"];
+	if (items["Titolo"].match("/")) {
+		title = items["Titolo"].substr(0,items["Titolo"].indexOf("/"));  
+	}
 	newItem.title = title;	
-  }
+	}
 }
 
 function getPages(newItem, items) {
-  if (items["Descrizionefisica"]) {
-  	pages = items["Descrizionefisica"].substr(0,items["Descrizionefisica"].indexOf("p")-1);  
+	if (items["Descrizionefisica"]) {
+	pages = items["Descrizionefisica"].substr(0,items["Descrizionefisica"].indexOf("p")-1);  
 	newItem.numPages = pages;	
-  }
+	}
 }
 
 function getEdition(newItem, items) {
-  if (items["Edizione"]) {
-  	edition = items["Edizione"];
+	if (items["Edizione"]) {
+	edition = items["Edizione"];
 	newItem.edition = edition.replace(/\D/g,'');	
-  }
+	}
 }
 
 function getImprints(newItem, items) {
@@ -138,60 +138,60 @@ function getImprints(newItem, items) {
   if (items["Pubblicazione"]) {
 	items["Pubblicazione"] = items["Pubblicazione"].replace(/\s\s+/g, '');
 	if (items["Pubblicazione"].match(":")) {
-	  var colonLoc = items["Pubblicazione"].indexOf(":");
-	  place = items["Pubblicazione"].substr(0, colonLoc-1);
-	  newItem.place = place.replace(/\W/g, '');
-	  var commaLoc = items["Pubblicazione"].lastIndexOf(",");
-	  var date1 =items["Pubblicazione"].substr(commaLoc + 1);
-	  date1 = date1.substr(0, date1.length);
-	  newItem.date = date1.replace(/\D/g, '');
-	  newItem.publisher = items["Pubblicazione"].substr(colonLoc+1, commaLoc-colonLoc-1);
+		var colonLoc = items["Pubblicazione"].indexOf(":");
+		place = items["Pubblicazione"].substr(0, colonLoc-1);
+		newItem.place = place.replace(/\W/g, '');
+		var commaLoc = items["Pubblicazione"].lastIndexOf(",");
+		var date1 =items["Pubblicazione"].substr(commaLoc + 1);
+		date1 = date1.substr(0, date1.length);
+		newItem.date = date1.replace(/\D/g, '');
+		newItem.publisher = items["Pubblicazione"].substr(colonLoc+1, commaLoc-colonLoc-1);
 	} else {
-	  newItem.publisher = items["Pubblicazione"];
+		newItem.publisher = items["Pubblicazione"];
 	}
   }
 }
 
 function saveToZotero(newItem, items) {
-  // Associate and save well-formed data to Zotero
-  associateData (newItem, items, "Title:", "title");
-  //associateData (newItem, items, "ISBN-10:", "ISBN");
-  //associateData (newItem, items, "Collection:", "extra");
-  associateData (newItem, items, "Pages:", "pages");
-  newItem.repository = "OPAC SBN";
-  newItem.complete();
+	// Associate and save well-formed data to Zotero
+	associateData (newItem, items, "Title:", "title");
+	//associateData (newItem, items, "ISBN-10:", "ISBN");
+	//associateData (newItem, items, "Collection:", "extra");
+	associateData (newItem, items, "Pages:", "pages");
+	newItem.repository = "OPAC SBN";
+	newItem.complete();
 }
 
 function doWeb(doc, url) {
-  // namespace code
-  var namespace = doc.documentElement.namespaceURI;
-  var nsResolver = namespace ? function(prefix) {
+	// namespace code
+	var namespace = doc.documentElement.namespaceURI;
+	var nsResolver = namespace ? function(prefix) {
 	if (prefix == 'x') return namespace; else return null;
-  } : null;
+	} : null;
 
-  // variable declarations
-  var articles = new Array();
-  var items = new Object();
-  var nextTitle;
+	// variable declarations
+	var articles = new Array();
+	var items = new Object();
+	var nextTitle;
 
-  // If Statement checks if page is a Search Result, then saves requested Items
-  if (detectWeb(doc, url) == "multiple") {
-	var titles = doc.evaluate('//td[2]/a', doc, nsResolver, XPathResult.ANY_TYPE, null);
+	// If Statement checks if page is a Search Result, then saves requested Items
+	if (detectWeb(doc, url) == "multiple") {
+	var titles = doc.evaluate('//tbody/tr/td[3]/div[2]/a', doc, nsResolver, XPathResult.ANY_TYPE, null);
 	while (nextTitle = titles.iterateNext()) {
-	  items[nextTitle.href] = nextTitle.textContent;
+		items[nextTitle.href] = nextTitle.textContent;
 	}
 	items = Zotero.selectItems(items);
 	for (var i in items) {
-	  articles.push(i);
+		articles.push(i);
 	}
-  } else {
+	} else {
 	//saves single page items
 	articles = [url];
-  }
+	}
 
-  // process everything, calling function=scrape to do the heavy lifting
-  Zotero.Utilities.processDocuments(articles, scrape, function(){Zotero.done();});
-  Zotero.wait();
+	// process everything, calling function=scrape to do the heavy lifting
+	Zotero.Utilities.processDocuments(articles, scrape, function(){Zotero.done();});
+	Zotero.wait();
 }
 /** BEGIN TEST CASES **/
 var testCases = [
